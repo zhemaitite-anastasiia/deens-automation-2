6b5fef6b6033bc3653954f9c48103a78f79cd3c1
kamil <kambagautdinov@gmail.com>
2019-08-05 15:17:54 -0700

    Added @BeforeMEthod and @AfterMethod. Style adjustments.

--- src/test/java/io/testpro/deens/LandingTests.java
+++ src/test/java/io/testpro/deens/LandingTests.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.*;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.xml.*;
 
@@ -17,9 +19,20 @@ import java.util.List;
 
 public class LandingTests {
 
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    private void afterMethodSetup() {
+        driver.quit();
+    }
+
     @Test
     public void titleIsCorrect() {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://deens-master.now.sh/");
         driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
         WebElement element = (new WebDriverWait(driver, 20))
@@ -30,47 +43,43 @@ public class LandingTests {
 
     @Test
     private void headerIsLoaded() {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://deens-master.now.sh/");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Assert.assertTrue(driver.findElement(By.cssSelector("header[class^=\"TopBar\"]")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("header[class^='TopBar']")).isDisplayed());
         driver.quit();
     }
 
     @Test
     private void logoIsVisible() {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://deens-master.now.sh/");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Assert.assertTrue(driver.findElement(By.cssSelector("div[class^=\"Logo\"")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("div[class^='Logo'")).isDisplayed());
         driver.quit();
     }
 
     // Regular view: verify the top menu navigation bar is loaded
     @Test
     private void navBarIsVisible() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://deens-master.now.sh/");
         //Maximizing the screen size
         driver.manage().window().maximize();
+        driver.get("https://deens-master.now.sh/");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Assert.assertTrue(driver.findElement(By.cssSelector("div[class^=\"DesktopNav\"")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("div[class^='DesktopNav'")).isDisplayed());
         driver.quit();
     }
 
     // Mobile view: verify the top menu navigation bar is NOT loaded
     @Test
     private void mobile_NavBarIsNotVisible() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://deens-master.now.sh/");
         // Setting the screen size to Iphone X parameters
         driver.manage().window().setSize(new Dimension(375, 812));
+        driver.get("https://deens-master.now.sh/");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         // headerIsFound = true, if the menu is visible, headerIsFound = false - otherwise
         boolean headerIsFound = true;
         try {
-            driver.findElement(By.cssSelector("div[class^=\"DesktopNav\""));
+            driver.findElement(By.cssSelector("div[class^='DesktopNav'"));
         } catch(NoSuchElementException ex) {
             headerIsFound = false;
             System.out.println("NoSuchElementException exception is thrown.");
@@ -83,7 +92,6 @@ public class LandingTests {
     // Verify trip creator name link
     @Test
     public void tripCreatorNameCheckTest() {
-        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://deens-master.now.sh/");
         WebDriverWait wait = new WebDriverWait(driver, 10);
@@ -107,12 +115,9 @@ public class LandingTests {
     //Verify the list of available creator trips
     @Test
     public void listOfCreatorTripsCheckTest() {
-        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         WebDriverWait wait = new WebDriverWait(driver, 10);
         driver.get("https://deens-master.now.sh/");
-
-
         //click right carousel button
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[class*='Carousel__ButtonRight']")));
         WebElement rightCreatorCarouselButton = driver.findElements(By.cssSelector("button[class*='Carousel__ButtonRight']")).get(1);
@@ -129,7 +134,6 @@ public class LandingTests {
     //Verify that clicking on creators trip name redirects to the trip details page
     @Test
     public void creatorTripNameCheckTest() {
-        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         WebDriverWait wait = new WebDriverWait(driver, 10);
         driver.get("https://deens-master.now.sh/");
