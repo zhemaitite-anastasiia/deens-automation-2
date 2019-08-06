d3e21ef966922fd4c9aacdc06b46dd3c010b071d
Azat Zakuanov <azat.zakuanov@thrivemarket.com>
2019-08-06 14:40:00 -0700

    new tests for login

--- src/test/java/io/testpro/deens/Login.java
+++ src/test/java/io/testpro/deens/Login.java
@@ -39,6 +39,20 @@ public class Login {
     }
 
     @Test
+    public void LoginEmptyEmailTest2(){
+        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://deens-master.now.sh/login");
+        driver.findElement(By.cssSelector("#password")).sendKeys("qwerty");
+        driver.findElement(By.cssSelector("[data-testid='loginSubmit']")).click();
+
+        Assert.assertTrue(driver.findElement(By.cssSelector(".ui.error.message")).isDisplayed());
+
+        driver.quit();
+    }
+
+    @Test
     public void LoginEmptyPasswordlTest(){
         WebDriver driver = new ChromeDriver();
 //        driver.manage().window().maximize();
