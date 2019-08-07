3c3169df7df5ede8614a494429078b87b1d9949e
mogreat1 <mogreat1@gmail.com>
2019-08-07 19:12:19 +0200

    deleting sout line

--- src/test/java/io/testpro/deens/MyProfileTests.java
+++ src/test/java/io/testpro/deens/MyProfileTests.java
@@ -20,7 +20,7 @@ public class MyProfileTests {
     private String password = "Password1";
 
     @BeforeMethod
-    public void setUpAndAlogin() {
+    public void setUpAndLogin() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -41,13 +41,13 @@ public class MyProfileTests {
         String bioText = "Test Info about User";
         driver.findElement(By.xpath("//*[@name='biography']")).sendKeys(bioText);
         driver.findElement(By.xpath("//*[text()='Save']")).click();
+
         SoftAssert softAssert = new SoftAssert();
         List<WebElement> textField = driver.findElements(By.xpath("//*[@name='biography']"));
         softAssert.assertTrue(textField.size() == 0);
         List<WebElement> saveButton = driver.findElements(By.xpath("//*[text()='Save']"));
         softAssert.assertTrue(saveButton.size() == 0);
         softAssert.assertTrue(driver.findElement(By.xpath("//span[contains(@class,'Profile__Horizontal')]/p")).getText().contains(bioText));
-        System.out.println(driver.findElement(By.xpath("//span[contains(@class,'Profile__Horizontal')]/p")).getText());
         softAssert.assertAll();
 
     }
@@ -56,6 +56,7 @@ public class MyProfileTests {
     public void openReviewPageTest() {
         driver.findElement(By.xpath("//*[contains(@class,'UserBasicInfo__Name')]")).click();
         String reviewText = driver.findElement(By.xpath("//*[text()='Reviews given to the user']")).getText();
+
         Assert.assertEquals(reviewText, "Reviews given to the user");
 
     }
@@ -63,6 +64,7 @@ public class MyProfileTests {
     @Test(enabled = true, description = "PC-106 Verify that user can see his/her Profile info - Version 1")
     public void profileInfoTest() {
         String profileTitle = driver.findElement(By.xpath("//*[text()='Biography:']")).getText();
+        
         Assert.assertEquals(profileTitle, "Biography:");
 
     }
