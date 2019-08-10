d68fd63d31f7127edd446b819142260c9b0dc894
mogreat1 <mogreat1@gmail.com>
2019-08-10 16:43:56 +0200

    Correction of Assertion after Vasil CR

--- src/test/java/io/testpro/deens/MyProfileTests.java
+++ src/test/java/io/testpro/deens/MyProfileTests.java
@@ -18,6 +18,8 @@ public class MyProfileTests {
     WebDriver driver;
     private String login = "mogreat1@gmail.com";
     private String password = "Password1";
+    private static By biography = By.xpath("//*[@name='biography']");
+    private static By save = By.xpath("//*[text()='Save']");
 
     @BeforeMethod
     public void setUpAndLogin() {
@@ -37,18 +39,20 @@ public class MyProfileTests {
     @Test(enabled = true, description = "PC-37 Verify ability to add Bio information in your Profile using Letters only")
     public void addBioInfoTest() {
         driver.findElement(By.xpath("//*[@class='pencil icon']")).click();
-        WebElement biographyTestField = driver.findElement(By.xpath("//*[@name='biography']"));
+        WebElement biographyTestField = driver.findElement(biography);
         biographyTestField.clear();
         String bioText = "Test Info about User";
         biographyTestField.sendKeys(bioText);
-        driver.findElement(By.xpath("//*[text()='Save']")).click();
+        driver.findElement(save).click();
 
         SoftAssert softAssert = new SoftAssert();
-        List<WebElement> textField = driver.findElements(By.xpath("//*[@name='biography']"));
+        List<WebElement> textField = driver.findElements(biography);
         softAssert.assertTrue(textField.size() == 0);
-        List<WebElement> saveButton = driver.findElements(By.xpath("//*[text()='Save']"));
+
+        List<WebElement> saveButton = driver.findElements(save);
         softAssert.assertTrue(saveButton.size() == 0);
         softAssert.assertTrue(driver.findElement(By.xpath("//span[contains(@class,'Profile__Horizontal')]/p")).getText().contains(bioText));
+
         softAssert.assertAll();
 
     }
@@ -56,18 +60,17 @@ public class MyProfileTests {
     @Test(enabled = true, description = "PC-103 User can open Review Page - Version 1")
     public void openReviewPageTest() {
         driver.findElement(By.xpath("//*[contains(@class,'UserBasicInfo__Name')]")).click();
-        String reviewText = driver.findElement(By.xpath("//*[text()='Reviews given to the user']")).getText();
 
-        Assert.assertEquals(reviewText, "Reviews given to the user");
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()='Reviews given to the user']")).isDisplayed());
 
     }
 
     @Test(enabled = true, description = "PC-106 Verify that user can see his/her Profile info - Version 1")
     public void profileInfoTest() {
-        String profileTitle = driver.findElement(By.xpath("//*[text()='Biography:']")).getText();
+
         SoftAssert softAssert = new SoftAssert();
-        softAssert.assertEquals(profileTitle, "Biography:");
-        softAssert.assertEquals(driver.getCurrentUrl(), "https://deens-master.now.sh/account/profile");
+        softAssert.assertTrue(driver.findElement(By.xpath("//h2[text()='Profile']")).isDisplayed());
+        softAssert.assertEquals(driver.getCurrentUrl(), "https://deens-master.now.sh/my/profile");
         softAssert.assertAll();
 
     }
