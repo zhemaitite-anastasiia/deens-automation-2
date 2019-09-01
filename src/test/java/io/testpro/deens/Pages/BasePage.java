8ad8154cd23f440b9028084dbf2beb48fe348afa
Anastasia <anasteisha170831@gmail.com>
2019-09-01 20:00:06 +0000

    Update BasePage.java

--- src/test/java/io/testpro/deens/Pages/BasePage.java
+++ src/test/java/io/testpro/deens/Pages/BasePage.java
@@ -1,5 +1,4 @@
 package io.testpro.deens.Pages;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,13 +7,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
-
     WebDriver driver;
     WebDriverWait wait;
 
-
     public BasePage(WebDriver driver) {
-
         this.driver = driver;
         wait =  new WebDriverWait(driver,10);
         PageFactory.initElements(driver, this);
@@ -22,20 +18,17 @@ public class BasePage {
 
     public WebElement waitUntilClickable(By locator) {
         return wait.until(ExpectedConditions.elementToBeClickable(locator));
-
     }
+    
     public WebElement waitUntilVisibility(WebElement element) {
         return wait.until(ExpectedConditions.visibilityOf(element));
-
     }
-
 
     public void typeText(String cssLocator, String value) {
         WebElement element = waitUntilClickable(By.cssSelector(cssLocator));
         element.click();
         element.sendKeys(value);
     }
-
 
     public void clickToElement(String locator) {
         WebElement element = waitUntilClickable(By.cssSelector(locator));
