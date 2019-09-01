0f980f301742e58f384125205cfb4bfcf1114534
Anastasia <anasteisha170831@gmail.com>
2019-09-01 20:01:21 +0000

    Update BaseTest.java

--- src/test/java/io/testpro/deens/BaseTest.java
+++ src/test/java/io/testpro/deens/BaseTest.java
@@ -1,16 +1,13 @@
 package io.testpro.deens;
-
 import io.testpro.deens.Pages.PDPPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
-
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-
    public  WebDriver driver;
     WebDriverWait wait;
 
@@ -19,16 +16,13 @@ public class BaseTest {
         System.setProperty("webdriver.chrome.driver", "src/test/java/chromedriver");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //driver.manage().window().maximize();
         wait = new WebDriverWait(driver,10);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().fullscreen();
-
     }
 
     @AfterClass
     public void tearDown() {
         driver.quit();
     }
-
 }
